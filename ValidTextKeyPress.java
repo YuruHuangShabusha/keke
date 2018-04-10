67819d3b9eb0c84bfb8887efd4f95cf78ea5a194
YuruHuangShabusha <35787537+YuruHuangShabusha@users.noreply.github.com>
2018-04-10 18:57:11 -0400

    Add files via upload

---
+++ ValidTextKeyPress.java
@@ -0,0 +1,26 @@
+package edu.nyu.cs9053.homework8;
+
+public enum ValidTextKeyPress{
+	Two(new char[] {'a', 'b', 'c', 'A', 'B', 'C'}),
+	Three(new char[] {'d', 'e', 'f', 'D', 'E', 'F'}),
+	Four(new char[] {'g', 'h', 'i', 'G', 'H', 'I'}),
+	Five(new char[] {'j', 'k', 'l', 'J', 'K', 'L'}),
+	Six(new char[] {'m', 'n', 'o', 'M', 'N', 'O'}),
+	Seven(new char[] {'p', 'q', 'r', 's', 'P', 'Q', 'R', 'S'}),
+	Eight(new char[] {'t', 'u', 'v', 'T', 'U', 'V'}),
+	Nine(new char[] {'w', 'x', 'y', 'z', 'W', 'X', 'Y', 'Z'});
+	
+	private final char[] characters;
+	
+	ValidTextKeyPress(char[] characters){
+		this.characters = characters;
+	}
+	
+	public char[] getCharacters(){
+		return characters;
+	}
+	
+	
+	
+	
+}
